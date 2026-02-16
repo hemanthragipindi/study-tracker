91fa0557dbb1ccd4d9b8a13317e1ffc8bf955471
Hemanth Reddy <ragipindihemanth713@gmail.com>
2026-02-17 03:17:41 +0530

    Removed node-fetch (using built-in fetch)

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-const fetch = require("node-fetch");
 
 const app = express();
 
